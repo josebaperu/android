81acbf04fe5ab1eac689081267cb37b5e776f186
jose <josetorresgarrido@gmail.com>
2023-06-06 10:20:25 -0500

    youtube-app-not-sticky

--- app/src/main/java/com/webview/youtube/service/WebViewService.java
+++ app/src/main/java/com/webview/youtube/service/WebViewService.java
@@ -69,7 +69,7 @@ public class WebViewService extends Service {
                 startForeground(1, notification);
             }
         }
-        return START_STICKY;
+        return START_NOT_STICKY;
     }
 
     @SuppressLint("WakelockTimeout")
