566de8075cae3c12a8d76895b704f39740fc6e7e
jose <josetorresgarrido@gmail.com>
2023-06-09 23:34:20 -0500

    youtube update scrp

--- app/src/main/res/raw/continuewatching.js
+++ app/src/main/res/raw/continuewatching.js
@@ -1,7 +1,6 @@
 const yesButton = document.querySelector("button.yt-spec-button-shape-next.yt-spec-button-shape-next--text.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--size-m[aria-label='Yes']");
 setInterval(() => {
-    if(!!yesButton) {
+    if(document.querySelectorAll("ytm-button-renderer")[5]?.childNodes[0]?.textContent === "Yes" && !!yesButton) {
         yesButton.click();
-        console.log("unpaused");
     }
 }, 100);
