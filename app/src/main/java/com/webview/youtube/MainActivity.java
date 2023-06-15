1b64f3a4adf0f5419b16418f5c68e99d480d0cad
jose <josetorresgarrido@gmail.com>
2023-06-14 19:33:28 -0500

    YOUTUBE: slim

--- app/src/main/java/com/webview/youtube/MainActivity.java
+++ app/src/main/java/com/webview/youtube/MainActivity.java
@@ -12,8 +12,8 @@ import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
-import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
@@ -38,34 +38,34 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final static WebResourceResponse webResourceResponse = new WebResourceResponse("text/plain", "utf-8", new ByteArrayInputStream("".getBytes()));
     private MediaWebView mWebView;
-    private StringBuilder adservers;
     private StringBuilder youtubeAds;
     private StringBuilder continueWatching;
     private StringBuilder scroll;
     private final Activity mainActivity = this; // If you are in activity
     private final static String UA = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36";
     public final static String RECEIVER = "YOUTUBE";
-    private boolean isImmersive = false;
-
+    private final static String BASE_URL = "https://www.youtube.com/";
+    private final static String LOG = "YouTube";
     private BroadcastReceiver receiver;
-
-
+    private List<String> whiteHostList;
     private void startService() {
         Intent serviceIntent = new Intent(this, WebViewService.class);
         serviceIntent.setAction("START");
         ContextCompat.startForegroundService(this, serviceIntent);
         registerReceiver(receiver, new IntentFilter(RECEIVER));
-
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        whiteHostList = getWhiteHostList();
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -91,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(getResources().getColor(R.color.black));
         }
         startService();
-        adservers = fileToSb(R.raw.adblockserverlist);
         youtubeAds = fileToSb(R.raw.noadsyoutube);
         continueWatching = fileToSb(R.raw.continuewatching);
         scroll = fileToSb(R.raw.scroll);
@@ -124,13 +123,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                ByteArrayInputStream EMPTY = new ByteArrayInputStream("".getBytes());
-                String kk5 = String.valueOf(adservers);
 
-                if (kk5.contains(":::::" + request.getUrl().getHost())) {
-                    return new WebResourceResponse("text/plain", "utf-8", EMPTY);
+                String hostRequest = request.getUrl().getHost();
+                boolean isAllowed = false;
+                for(String whiteListHost : whiteHostList) {
+                    if(hostRequest.contains(whiteListHost)){
+                        Log.d(LOG, "NOTINTERCEPTED :" + hostRequest);
+                        isAllowed = true;
+                        break;
+                    } else {
+                        Log.d(LOG, "INTERCEPTED :" + hostRequest);
+                    }
                 }
-                return super.shouldInterceptRequest(view, request);
+                return isAllowed ? super.shouldInterceptRequest(view, request) :  webResourceResponse;
             }
         });
         mWebView.setWebChromeClient(new WebChromeClient() {
@@ -194,24 +199,25 @@ public class MainActivity extends AppCompatActivity {
         }
         mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         mWebView.setScrollbarFadingEnabled(false);
-
-        if(isImmersive) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                getWindow().getInsetsController().hide(WindowInsets.Type.systemBars());
-            } else {
-                int uiVisibility = mainActivity.getWindow().getDecorView().getSystemUiVisibility();
-
-                uiVisibility |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
-                uiVisibility |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-                uiVisibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-                uiVisibility |= View.SYSTEM_UI_FLAG_IMMERSIVE;
-                uiVisibility |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-                mainActivity.getWindow().getDecorView().setSystemUiVisibility(uiVisibility);
-            }
-        }
         mWebView.loadUrl(getValue("url"));
     }
 
+    private List<String>  getWhiteHostList() {
+        String line = "";
+        List<String> list = new ArrayList<>();
+
+        InputStream is = this.getResources().openRawResource(R.raw.whitelisthosts);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        try {
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
     private StringBuilder fileToSb(int resource) {
         String line = "";
         StringBuilder sb = new StringBuilder();
@@ -280,7 +286,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private String getValue(String key) {
-        return getSharedPreferences().getString(key, "https://www.youtube.com");
+        return getSharedPreferences().getString(key, BASE_URL);
     }
 
     private SharedPreferences getSharedPreferences() {
