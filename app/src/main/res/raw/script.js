60d7303794a0dbc4f17fc1e2b2d7fccfbce0fac5
jose <josetorresgarrido@gmail.com>
2023-06-30 09:35:37 -0500

    spotify: script update

--- app/src/main/res/raw/script.js
+++ app/src/main/res/raw/script.js
@@ -1,30 +1,33 @@
-
+    const PLAYING = "Playing : ";
     window.setInterval (() => {
         const play = document.querySelector("[data-testid='play-button']");
         if(localStorage.getItem("reload") === "true") {
             if(!!play) {
-                play.click();
- 				console.log(".play");
-				localStorage.removeItem("reload");
-
+                setTimeout(function() {
+                    play.click();
+ 				    console.log(PLAYING + "AD SKIPPED");
+				    localStorage.removeItem("reload");
+                }, 1800);
             } else  {
-				console.log(".else");
+				console.log(PLAYING + "NULL");
             }
-        }
-        const ad = document.querySelector("[data-testid='npv-header-title']");
-
-        if(!!ad  &&'Advertisement' === ad.textContent) {
-            localStorage.setItem("reload", "true");
-            window.location.reload();
-            window.clearInterval(this);
-        }
-        const floatingPlayer = document.querySelectorAll("[data-testid='floating-now-playing-bar'] span");
-        if(!!floatingPlayer && floatingPlayer.length === 2) {
-            console.log("Playing : " + floatingPlayer[1].innerText + " - " + floatingPlayer[0].innerText);
         } else {
-            const tunePlayer = document.querySelectorAll("[data-testid='npv-metadata-container'] a");
-            if(!!tunePlayer && tunePlayer.length === 2) {
-                console.log("Playing : " + tunePlayer[1].innerText + " - " + tunePlayer[0].innerText);
+            const ad = document.querySelector("[data-testid='npv-header-title']");
+
+            if(!!ad  &&'Advertisement' === ad.textContent) {
+                localStorage.setItem("reload", "true");
+                window.location.reload();
+                window.clearInterval(this);
+            }
+            const floatingPlayer = document.querySelectorAll("[data-testid='floating-now-playing-bar'] span");
+            if(!!floatingPlayer && floatingPlayer.length === 2) {
+                console.log(PLAYING + floatingPlayer[1].innerText + " - " + floatingPlayer[0].innerText);
+            } else {
+                const tunePlayer = document.querySelectorAll("[data-testid='npv-metadata-container'] a");
+                if(!!tunePlayer && tunePlayer.length === 2) {
+                    console.log(PLAYING + tunePlayer[1].innerText + " - " + tunePlayer[0].innerText);
+                }
             }
         }
+
     }, 1000);
