ca352c1560994e756ad6103f56b472d6e73dac06
jose <josetorresgarrido@gmail.com>
2023-08-19 20:31:39 -0500

    tvnowmobile

--- app/src/main/java/com/webview/telegratishd/MainActivity.java
+++ app/src/main/java/com/webview/telegratishd/MainActivity.java
@@ -106,6 +106,13 @@ public class MainActivity extends AppCompatActivity {
         mWebView = findViewById(R.id.activity_main_webview);
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
+            public boolean shouldOverrideUrlLoading (WebView view, String url) {
+                if (url.contains("telegratishd")) {
+                    return false;
+                }
+                return true;
+            }
+            @Override
             public void onPageFinished(WebView view, String url) {
                 applyStyles();
             }
