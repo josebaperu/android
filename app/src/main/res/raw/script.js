b517d680816b303124457b7eaba5966fdf3fe7c1
jose <josetorresgarrido@gmail.com>
2023-08-28 19:46:47 -0500

    ytm-ua

--- app/src/main/res/raw/script.js
+++ app/src/main/res/raw/script.js
@@ -1,14 +1,11 @@
 setInterval(() => {
     window.alert = function() {};
-    window.onbeforeunload = null;
 
 
     if(Array.from(document.querySelectorAll("ytm-button-renderer")).filter( x => !!x.childNode && x.childNode[0] === "Yes")) {
         let yesButton = document.querySelector("button.yt-spec-button-shape-next.yt-spec-button-shape-next--text.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--size-m[aria-label='Yes']");
         if(!!yesButton) {
             yesButton.click();
-            console.log("click yesButton");
-
         }
     }
 
