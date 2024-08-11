1857a05b6566c0a921fbe1bcf69a90a367d0c33a
joseTG <josetorresgarrido@gmail.com>
2024-08-11 01:45:55 -0500

    master squash

---
+++ README.md
@@ -0,0 +1,3 @@
+# Android-Webview with Service
+
+JDK is version 11 (android studio)
---
+++ app/src/main/java/com/webview/google/MainActivity.java
@@ -0,0 +1,294 @@
+package com.webview.google;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Base64;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.ConsoleMessage;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
+
+import com.webview.google.service.WebViewService;
+import com.webview.google.webview.MediaWebView;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private final static WebResourceResponse webResourceResponse = new WebResourceResponse("text/plain", "utf-8", new ByteArrayInputStream("".getBytes()));
+    private MediaWebView mWebView;
+    private final Activity mainActivity = this; // If you are in activity
+    public final static String RECEIVER = "GOOGLE";
+    private final static String BASE_URL = "https://www.google.com/";
+    private final static String LOG = "Google";
+    private List<String> blacklistedKeyword;
+
+    private String script;
+    private BroadcastReceiver receiver;
+    private void startService() {
+        Intent serviceIntent = new Intent(this, WebViewService.class);
+        serviceIntent.setAction("START");
+        ContextCompat.startForegroundService(this, serviceIntent);
+        registerReceiver(receiver, new IntentFilter(RECEIVER));
+    }
+    private void handleObserverDestroy () {
+        save("url", mWebView.getUrl());
+        unregisterReceiver(receiver);
+        finishAndRemoveTask();
+        receiver = null;
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                handleObserverDestroy();
+            }
+        };
+        /*
+        blacklistedKeyword = new ArrayList<>();
+        blacklistedKeyword.add(".mp3");
+        blacklistedKeyword.add("log");
+        blacklistedKeyword.add("play.google");
+        blacklistedKeyword.add("stats");
+        blacklistedKeyword.add("adview");
+        blacklistedKeyword.add("generate");
+        blacklistedKeyword.add("googleads");
+        blacklistedKeyword.add("pagead");
+        blacklistedKeyword.add("doubleclick.net");
+        blacklistedKeyword.add("track");
+        */
+        blacklistedKeyword = new ArrayList<>();
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black, this.getTheme()));
+        } else {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+        }
+        startService();
+        script = fileToStr(R.raw.script);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_main);
+
+        mWebView = new MediaWebView(MainActivity.this);
+        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void doUpdateVisitedHistory (WebView view,
+                                                String url,
+                                                boolean isReload) {
+
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //injectCSS();
+                //mWebView.loadUrl("javascript:(function() { " +
+                //        script +
+                //       ";})()");
+
+            }
+            private void injectCSS() {
+                try {
+                    InputStream inputStream = getAssets().open("style.css");
+                    byte[] buffer = new byte[inputStream.available()];
+                    inputStream.read(buffer);
+                    inputStream.close();
+                    String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+                    mWebView.loadUrl("javascript:(function() {" +
+                            "var parent = document.getElementsByTagName('head').item(0);" +
+                            "var style = document.createElement('style');" +
+                            "style.type = 'text/css';" +
+                            // Tell the browser to BASE64-decode the string into your script !!!
+                            "style.innerHTML = window.atob('" + encoded + "');" +
+                            "parent.appendChild(style)" +
+                            "})()");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                /*
+                String url = request.getUrl().toString();
+                boolean isAllowed = false;
+                for(String blacklistedWord : blacklistedKeyword) {
+                    if(!url.contains(blacklistedWord)){
+                        isAllowed = true;
+                    }
+                }
+
+                return isAllowed ? super.shouldInterceptRequest(view, request) :  webResourceResponse;
+                */
+                return super.shouldInterceptRequest(view, request);
+            }
+        });
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            private View mCustomView;
+            private WebChromeClient.CustomViewCallback mCustomViewCallback;
+            private int mOriginalOrientation;
+            private int mOriginalSystemUiVisibility;
+
+            public Bitmap getDefaultVideoPoster() {
+                if (mainActivity == null) {
+                    return null;
+                }
+                return BitmapFactory.decodeResource(mainActivity.getApplicationContext().getResources(), 2130837573);
+            }
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                android.util.Log.d("JS_CONSOLE", consoleMessage.message());
+                return true;
+            }
+            @Override
+            public void onHideCustomView() {
+                ((FrameLayout) mainActivity.getWindow().getDecorView()).removeView(this.mCustomView);
+                this.mCustomView = null;
+                mainActivity.getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+                mainActivity.setRequestedOrientation(this.mOriginalOrientation);
+                this.mCustomViewCallback.onCustomViewHidden();
+                this.mCustomViewCallback = null;
+            }
+            @Override
+            public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback) {
+                if (this.mCustomView != null) {
+                    onHideCustomView();
+                    return;
+                }
+                this.mCustomView = paramView;
+                this.mOriginalSystemUiVisibility = mainActivity.getWindow().getDecorView().getSystemUiVisibility();
+                this.mOriginalOrientation = mainActivity.getRequestedOrientation();
+                this.mCustomViewCallback = paramCustomViewCallback;
+                ((FrameLayout) mainActivity.getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+                mainActivity.getWindow().getDecorView().setSystemUiVisibility(3846);
+            }
+        });
+        CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
+        WebSettings webSettings = mWebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(false);   // Enable this only if you want pop-ups!
+        webSettings.setMediaPlaybackRequiresUserGesture(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setBlockNetworkLoads(false);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            WebSettingsCompat.setForceDark(webSettings, WebSettingsCompat.FORCE_DARK_ON);
+        }
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setScrollbarFadingEnabled(false);
+        mWebView.loadUrl(getValue("url"));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.clear();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView != null && mWebView.canGoBack()){
+            mWebView.goBack();// if there is previous page open it
+        } else {
+            super.onBackPressed();//if there is no previous page, close app
+        }
+    }
+    @Override
+    public void onDestroy() {
+        handleObserverDestroy();
+        super.onDestroy();
+    }
+
+    private void save(String key, String value) {
+        SharedPreferences.Editor editor = getSharedPreferences().edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+    private void saveCurrentUrl (String url) {
+        save("url", url);
+    }
+
+
+    private String getValue(String key) {
+        return getSharedPreferences().getString(key, BASE_URL);
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(this);
+    }
+    private String fileToStr(int resource) {
+        String line = "";
+        StringBuilder sb = new StringBuilder();
+
+        InputStream is = this.getResources().openRawResource(resource);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        if (is != null) {
+            try {
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+}
