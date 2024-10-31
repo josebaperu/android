3903757c40bd0a82e26aec097abb77e2979d57af
joseTG <josetorresgarrido@gmail.com>
2024-10-30 22:26:54 -0500

    ytm-scripts-fix-setimeout

--- app/src/main/res/raw/noadsyoutube_source.js
+++ app/src/main/res/raw/noadsyoutube_source.js
@@ -1,55 +1,54 @@
-
-
 const observer = new MutationObserver(mutations => {
-    let search = document.querySelector("ytmusic-nav-bar[is-mweb]:not([user-logged-in]) .center-content.ytmusic-nav-bar");
-    let author = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(2)");
-    let track = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(1)");
-    let muteButton = document.querySelector('button.ytp-unmute.ytp-popup.ytp-button');
-    let dismissBtn = document.querySelector('yt-button-renderer.dismiss-button.style-scope.ytmusic-mealbar-promo-renderer > yt-button-shape > button > div');
-    let movie_player = document.querySelector('ytmusic-player#player');
-    let player_container = document.querySelector('#main-panel.ytmusic-player-page');
-    let skipButtons = document.querySelectorAll(".ytp-ad-skip-button");
-    let overlayAds = document.querySelectorAll(".ytp-ad-overlay-slot");
-  !!document.querySelectorAll('.image') && document.querySelectorAll('.image').length > 0 ? document.querySelectorAll('.image').forEach( i => i.style.display = 'none'): '';
-  if(!!search){
-    search.style.display = "block";
-  }
-
-  if (!!author && !!track) {
-      console.log("Playing : " + author.title + " - " + track.title);
-  }
-  if(!!muteButton && muteButton.style.display === '') {
-      muteButton.click();
-  }
-  if(!!dismissBtn){
-    dismissBtn.click();
-  }
-
-  if(!!movie_player) {
-      movie_player.style.display = 'none';
-  }
-
-
-  if(!!player_container){
-      player_container.style.height = 'unset';
-  }
-
-  let ad = document.querySelector('.ad-showing');
-
+  var ad = document.querySelector('.ad-showing');
   if (ad) {
-    let video = document.querySelector('video');
+    var video = document.querySelector('video');
 
     if (video) {
 		video.currentTime = video.duration;
 
-
-		for (let skipButton of skipButtons) {
+        var skipButtons = document.querySelectorAll(".ytp-ad-skip-button");
+		for (var skipButton of skipButtons) {
 		  skipButton.click();
 		}
     }
   }
-  for (let overlayAd of overlayAds) {
+  var overlayAds = document.querySelectorAll(".ytp-ad-overlay-slot");
+  for (var overlayAd of overlayAds) {
     overlayAd.style.visibility = "hidden";
   }
 });
 observer.observe(document, { subtree: true, childList: true });
+
+
+setTimeout(() => {
+    var search = document.querySelector("ytmusic-nav-bar[is-mweb]:not([user-logged-in]) .center-content.ytmusic-nav-bar");
+    var author = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(2)");
+    var track = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(1)");
+    var muteButton = document.querySelector('button.ytp-unmute.ytp-popup.ytp-button');
+    var dismissBtn = document.querySelector('yt-button-renderer.dismiss-button.style-scope.ytmusic-mealbar-promo-renderer > yt-button-shape > button > div');
+    var movie_player = document.querySelector('ytmusic-player#player');
+    var player_container = document.querySelector('#main-panel.ytmusic-player-page');
+      if(!!search){
+        search.style.display = "block";
+      }
+
+      if (!!author && !!track) {
+          console.log("Playing : " + author.title + " - " + track.title);
+      }
+      if(!!muteButton && muteButton.style.display === '') {
+          muteButton.click();
+      }
+      if(!!dismissBtn){
+        dismissBtn.click();
+      }
+
+      if(!!movie_player) {
+          movie_player.style.display = 'none';
+      }
+
+
+      if(!!player_container){
+          player_container.style.height = 'unset';
+      }
+
+}, 500);
