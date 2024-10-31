1a7106f12e883eecdbe8513d2d9de4b1d28e749c
joseTG <josetorresgarrido@gmail.com>
2024-10-30 23:53:28 -0500

    ytm-scripts-fix-setimeout

--- app/src/main/res/raw/noadsyoutube_source.js
+++ app/src/main/res/raw/noadsyoutube_source.js
@@ -1,54 +1,53 @@
-const observer = new MutationObserver(mutations => {
-  var ad = document.querySelector('.ad-showing');
-  if (ad) {
-    var video = document.querySelector('video');
-
-    if (video) {
-		video.currentTime = video.duration;
-
-        var skipButtons = document.querySelectorAll(".ytp-ad-skip-button");
-		for (var skipButton of skipButtons) {
-		  skipButton.click();
-		}
+setInterval(function() {
+    let author = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(2)");
+    let track = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(1)");
+    if (!!author && !!track) {
+        console.log("Playing : " + author.title + " - " + track.title);
     }
-  }
-  var overlayAds = document.querySelectorAll(".ytp-ad-overlay-slot");
-  for (var overlayAd of overlayAds) {
-    overlayAd.style.visibility = "hidden";
-  }
-});
-observer.observe(document, { subtree: true, childList: true });
+    let ad = document.querySelector('.ad-showing');
+    let video = document.querySelector('video');
+    let skipButtons = document.querySelectorAll(".ytp-ad-skip-button");
+    let overlayAds = document.querySelectorAll(".ytp-ad-overlay-slot");
+    let search = document.querySelector("ytmusic-nav-bar[is-mweb]:not([user-logged-in]) .center-content.ytmusic-nav-bar");
 
+    let muteButton = document.querySelector('button.ytp-unmute.ytp-popup.ytp-button');
+    let dismissBtn = document.querySelector('yt-button-renderer.dismiss-button.style-scope.ytmusic-mealbar-promo-renderer > yt-button-shape > button > div');
+    let movie_player = document.querySelector('ytmusic-player#player');
+    let player_container = document.querySelector('#main-panel.ytmusic-player-page');
+    if (ad) {
 
-setTimeout(() => {
-    var search = document.querySelector("ytmusic-nav-bar[is-mweb]:not([user-logged-in]) .center-content.ytmusic-nav-bar");
-    var author = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(2)");
-    var track = document.querySelector("yt-formatted-string.ytmusic-player-controls:nth-child(1)");
-    var muteButton = document.querySelector('button.ytp-unmute.ytp-popup.ytp-button');
-    var dismissBtn = document.querySelector('yt-button-renderer.dismiss-button.style-scope.ytmusic-mealbar-promo-renderer > yt-button-shape > button > div');
-    var movie_player = document.querySelector('ytmusic-player#player');
-    var player_container = document.querySelector('#main-panel.ytmusic-player-page');
-      if(!!search){
+        if (video) {
+            video.currentTime = video.duration;
+
+            for (let skipButton of skipButtons) {
+                skipButton.click();
+            }
+        }
+    }
+    for (let overlayAd of overlayAds) {
+        if (overlayAd.style.visibility !== "hidden") {
+            overlayAd.style.visibility = "hidden";
+        }
+    }
+    if (!!search && search.style.display !== "block") {
         search.style.display = "block";
-      }
+    }
 
-      if (!!author && !!track) {
-          console.log("Playing : " + author.title + " - " + track.title);
-      }
-      if(!!muteButton && muteButton.style.display === '') {
-          muteButton.click();
-      }
-      if(!!dismissBtn){
+    if (!!muteButton) {
+        muteButton.click();
+    }
+    if (!!dismissBtn) {
         dismissBtn.click();
-      }
+    }
 
-      if(!!movie_player) {
-          movie_player.style.display = 'none';
-      }
+    if (!!movie_player && movie_player.style.display !== 'none') {
+        movie_player.style.display = 'none';
+    }
 
 
-      if(!!player_container){
-          player_container.style.height = 'unset';
-      }
+    if (!!player_container && player_container.style.height !== 'unset') {
+        player_container.style.height = 'unset';
+    }
+
 
 }, 500);
